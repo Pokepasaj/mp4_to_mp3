18391e123b183e53ddbd5b0b260bfeb9ca21d855
pokepasaj <pokepasaj@gmail.com>
2024-02-05 16:45:36 +0200

    added dockerfile

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -1,4 +1,4 @@
-import jwt, datatime, os
+import jwt, datetime, os
 from flask import Flask, request
 from flask_mysqldb import MySQL
 
@@ -12,13 +12,14 @@ server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
 server.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT"))
 
+
 @server.route("/login", methods=["POST"])
 def login():
     auth = request.authorization
     if not auth:
         return "missing credentials", 401
-    
-    # check db for username and password
+
+    # #User is going to make request to our login route using their username and passwd
     cur = mysql.connection.cursor()
     res = cur.execute(
         "SELECT email, password FROM user WHERE email=%s", (auth.username,)
@@ -28,3 +29,47 @@ def login():
         user_row = cur.fetchone()
         email = user_row[0]
         password = user_row[1]
+#checking to see if user data exists if it does then we can consider the user to be authenticated 
+        if auth.username != email or auth.password != password:
+            return "invalid credentials", 401
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True) #if authenticated this will return JWT(Jason Web Token)
+    else:         
+        #The JWT will be used by that user to make request to the API 
+        return "invalide credentials", 401
+
+#endpoint for the authservice to validate the JWTs
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+    if not encoded_jwt:
+        return "missing credentials", 401
+
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+
+    return decoded, 200
+
+#The endpoints of that the user which has JWT will be determined by the permission below(whether the user is an admin or not)
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
+
+#whenever we run our file using the python command we want our server to start
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000)
