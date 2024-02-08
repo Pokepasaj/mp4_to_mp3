82e995d9e465ad2991db7cffcc639458012bd0d5
pokepasaj <pokepasaj@gmail.com>
2024-02-08 11:33:53 +0200

    fixed typo in gateway server.py

--- python/src/gateway/storage/util.py
+++ python/src/gateway/storage/util.py
@@ -5,25 +5,25 @@ def upload(f, fs, channel, access):
     try:
         fid = fs.put(f)
     except Exception as err:
+        print(err)
         return "internal server error", 500
+
     message = {
         "video_fid": str(fid),
         "mp3_fid": None,
         "username": access["username"],
     }
-    
+
     try:
-        channel.basic.publish(
+        channel.basic_publish(
             exchange="",
-            routhing_key="video",
+            routing_key="video",
             body=json.dumps(message),
             properties=pika.BasicProperties(
-                delivery_mode=pika.spec.PERSISTANT_DELIVERY_MODE 
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
             ),
         )
-    except:
+    except Exception as err:
+        print(err)
         fs.delete(fid)
         return "internal server error", 500
-    
-    
-    
